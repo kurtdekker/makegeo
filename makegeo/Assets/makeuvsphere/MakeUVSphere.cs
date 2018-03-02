1d117a94aecb7f514d4d6447d7a78989b41881c0
Kurtina <kurtdekker@gmail.com>
2018-03-02 08:46:09 -0800

    MakeUVSphere: operational and makin' UV spheres, as intended

---
+++ makegeo/Assets/makeuvsphere/MakeUVSphere.cs
@@ -0,0 +1,101 @@
+﻿/*
+    The following license supersedes all notices in the source code.
+*/
+
+/*
+    Copyright (c) 2018 Kurt Dekker/PLBM Games All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+    
+    Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    
+    Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MakeUVSphere
+{
+	// dimensions is radial size in X,Y,Z (Y is polar axis)
+	// sectors are how many longitudinal dividers (full equatorial)
+	// meridians are how many latitudinal dividers (pole to pole)
+	public static GameObject Create( Vector3 dimensions, int sectors, int meridians)
+	{
+		GameObject go = new GameObject ("MakeUVSphere.Create();");
+
+		MeshFilter mf = go.AddComponent<MeshFilter> ();
+		Mesh mesh = mf.mesh;
+
+		List<Vector3> verts = new List<Vector3> ();
+		List<int> tris = new List<int> ();
+		List<Vector2> uvs = new List<Vector2> ();
+
+		for (int i = 0; i <= sectors; i++)
+		{
+			float longitude = (360.0f * i) / sectors;
+			for (int j = 0; j <= meridians; j++)
+			{
+				int n = verts.Count;
+
+				float latitude = -90 + (180.0f * j) / meridians;
+
+				Vector3 sphericalPoint = new Vector3 (
+					Mathf.Cos (longitude * Mathf.Deg2Rad) *
+						Mathf.Cos( latitude * Mathf.Deg2Rad) * dimensions.x,
+					Mathf.Sin( latitude * Mathf.Deg2Rad) * dimensions.y,
+					Mathf.Sin (longitude * Mathf.Deg2Rad) *
+						Mathf.Cos( latitude * Mathf.Deg2Rad) * dimensions.z);
+
+				verts.Add (sphericalPoint);
+
+				Vector2 uvPoint = new Vector2 ((float)i / sectors, (float)j / meridians);
+				uvs.Add (uvPoint);
+
+				if (i > 0 && j > 0)
+				{
+					tris.Add (n);
+					tris.Add (n - meridians - 1);
+					tris.Add (n - meridians);
+
+					tris.Add (n);
+					tris.Add (n - 1);
+					tris.Add (n - meridians - 1);
+				}
+			}
+		}
+
+		mesh.vertices = verts.ToArray ();
+		mesh.triangles = tris.ToArray ();
+		mesh.uv = uvs.ToArray ();
+
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+
+		go.AddComponent<MeshRenderer> ();
+
+		return go;
+	}
+}
