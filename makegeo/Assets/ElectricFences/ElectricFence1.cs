1f665f9cdd0bb5f4d3e0cce4d9c960da120686d1
Kurtina <kurtdekker@gmail.com>
2018-03-06 07:17:00 -0800

    Electric Fences: some ideas

---
+++ makegeo/Assets/ElectricFences/ElectricFence1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricFence1 : MonoBehaviour
+{
+	void OnDrawGizmos()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			Transform tr = transform.GetChild (i);
+
+			Gizmos.DrawWireSphere (tr.position, 0.5f);
+
+			if (tr.childCount == 2)
+			{
+				Transform a = tr.GetChild (0);
+				Transform b = tr.GetChild (1);
+
+				Gizmos.DrawLine (a.position, b.position);
+			}
+		}
+	}
+}
