88e43b9ebd061db2786560c876e831c5c195216c
Kurtina <kurtdekker@gmail.com>
2019-09-25 15:02:22 -0700

    MakePolygonCollider2D kicked off

---
+++ makegeo/Assets/2d/makepolygoncollider2d/testmakepolygoncollider2d.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmakepolygoncollider2d : MonoBehaviour
+{
+	void Start ()
+	{
+		Vector2[] v1 = new Vector2[] {
+			new Vector2( -1, 0),
+			new Vector2( 0, 1),
+			new Vector2( 1, 0),
+			new Vector2( 0, -1),
+		};
+
+		MakeCollider2D.Create( v1);	
+	}
+}
