62868a88e50c9c584d7783ed99a9e7014ea36de1
Kurtina <kurtdekker@gmail.com>
2020-05-05 07:52:50 -0700

    combine meshes - this is all junky stuff, unknown function as yet

---
+++ makegeo/Assets/CombineMeshes/junk/MC2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC2 : MonoBehaviour
+{
+	void Start ()
+	{
+		var filters = GetComponentsInChildren<MeshFilter>();	
+
+		List<Material> materials = new List<Material>();
+
+		List<CombineInstance> combines = new List<CombineInstance>();
+
+		for (int i = 0; i < filters.Length; i++)
+		{
+			var filter = filters[i];
+
+			var mesh = filter.sharedMesh;
+
+			var rndrr = filter.GetComponent<MeshRenderer>();
+
+			for (int j = 0; j < mesh.subMeshCount; j++)
+			{
+				var ci = new CombineInstance();
+
+				ci.mesh = mesh;
+				ci.subMeshIndex = j;
+
+				combines.Add( ci);
+
+				// I assume the end meshes are in linear order?
+				materials.Add( rndrr.materials[j]);
+			}
+		}
+
+		var go = new GameObject("combined");
+
+		go.transform.position += Vector3.right * 3;
+
+		var mf = go.AddComponent<MeshFilter>();
+		mf.mesh = new Mesh();
+		mf.mesh.CombineMeshes( combines.ToArray(), false);
+
+		var mr = go.AddComponent<MeshRenderer>();
+		mr.materials = materials.ToArray();
+
+		transform.position += Vector3.right * -3;
+	}
+}
---
+++ makegeo/Assets/CombineMeshes/junk/MeshCombiner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+	void Start()
+	{
+		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+		MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+		//Material[] SharedMats = meshRenderers[1].sharedMaterials;
+		Material MainMaterial = meshRenderers[1].materials[0];
+		Material[] SubMaterial = new Material[meshRenderers[1].materials.Length-1];
+		List<CombineInstance> combine2 = new List<CombineInstance>();
+
+		// gotta get this big enough
+		SubMaterial = new Material[2];
+
+		for (int i = 0; i < meshFilters.Length; i++)
+		{
+			Mesh mShared = meshFilters[i].sharedMesh;
+
+			combine[i].mesh = mShared;
+			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+
+			if (mShared.subMeshCount > 1)
+			{
+				// combine submeshes
+				for (int j = 0; j < mShared.subMeshCount; j++)
+				{
+					if (j < mShared.subMeshCount - 1)
+					{
+						SubMaterial[i] = meshRenderers[1].materials[0];
+					}
+					CombineInstance ci = new CombineInstance();
+
+					ci.mesh = mShared;
+					ci.subMeshIndex = j;
+					ci.transform = meshFilters[i].transform.localToWorldMatrix;
+
+					combine2.Add(ci);
+				}
+			}
+
+			meshFilters[i].gameObject.SetActive(false);
+		}
+		GameObject go = new GameObject(gameObject.name);
+		go.AddComponent<MeshFilter>();
+		go.AddComponent<MeshRenderer>();
+
+		var goFilter = go.GetComponent<MeshFilter>();
+		var goRenderer = go.GetComponent<MeshRenderer>();
+
+		go.SetActive(false);
+		goFilter.mesh = new Mesh();
+		goFilter.mesh.CombineMeshes(combine);
+		go.SetActive(true);
+
+		//goRenderer.materials = Mats;
+		goRenderer.material = MainMaterial;
+
+		if (SubMaterial.Length >= 1)
+		{
+			GameObject go2 = new GameObject(gameObject.name + "Sub");
+			go2.AddComponent<MeshFilter>();
+			go2.AddComponent<MeshRenderer>();
+
+			var goFilter2 = go2.GetComponent<MeshFilter>();
+			var goRenderer2 = go2.GetComponent<MeshRenderer>();
+
+			go2.SetActive(false);
+			goFilter2.mesh = new Mesh();
+			goFilter2.mesh.CombineMeshes(combine2.ToArray(), false);
+			go2.SetActive(true);
+
+			//goRenderer.materials = Mats;
+			goRenderer2.materials = SubMaterial;
+		}
+
+		Destroy(gameObject);
+	}
+}
