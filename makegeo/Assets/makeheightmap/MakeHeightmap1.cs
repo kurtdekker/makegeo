42f7527490fc3c3f8c336b583a4c09138a9050ad
Kurtina <kurtdekker@gmail.com>
2021-03-26 07:03:03 -0700

    MakeHeightmap1: quick and cheesy heightmaps

---
+++ makegeo/Assets/makeheightmap/MakeHeightmap1.cs
@@ -0,0 +1,109 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2021 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MakeHeightmap1
+{
+	public static GameObject Create( Texture2D HeightMap, Vector3 Dimensions)
+	{
+		GameObject go = new GameObject ("MakeHeightmap1.Create();");
+
+		MeshFilter mf = go.AddComponent<MeshFilter> ();
+		Mesh mesh = new Mesh();
+
+		List<Vector3> verts = new List<Vector3> ();
+		List<int> tris = new List<int> ();
+		List<Vector2> uvs = new List<Vector2> ();
+
+		int across = HeightMap.width;
+		int down = HeightMap.height;
+
+		for (int j = 0; j < down; j++)
+		{
+			for (int i = 0; i < across; i++)
+			{
+				Color rawColor = HeightMap.GetPixel( i, j);
+
+				float gray = rawColor.grayscale;
+
+				float y = gray * Dimensions.y;
+
+				float fx = (float)i / (across - 1);
+				float fz = (float)j / (down - 1);
+
+				Vector3 pos = new Vector3(
+					fx * Dimensions.x,
+					y,
+					fz * Dimensions.z);
+
+				pos.x -= Dimensions.x / 2;
+				pos.z -= Dimensions.z / 2;
+
+				int n = verts.Count;
+
+				verts.Add( pos);
+
+				uvs.Add( new Vector2( fx, fz));
+
+				if (i > 0 && j > 0)
+				{
+					tris.Add( n);
+					tris.Add( n - across);
+					tris.Add( n - (across + 1));
+
+					tris.Add( n);
+					tris.Add( n - (across + 1));
+					tris.Add( n - 1);
+				}
+			}
+		}
+
+		mesh.vertices = verts.ToArray ();
+		mesh.triangles = tris.ToArray ();
+		mesh.uv = uvs.ToArray ();
+
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+
+		mf.mesh = mesh;
+
+		go.AddComponent<MeshRenderer> ();
+
+		return go;
+	}
+}
---
+++ makegeo/Assets/makeheightmap/testmakeheightmap1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmakeheightmap1 : MonoBehaviour
+{
+	public Texture2D Heightmap;
+
+	public Vector3 Dimensions;
+
+	public Material mtl;
+
+	void Start ()
+	{
+		var go = MakeHeightmap1.Create( Heightmap, Dimensions);
+		go.GetComponent<Renderer>().material = mtl;
+	}
+}
