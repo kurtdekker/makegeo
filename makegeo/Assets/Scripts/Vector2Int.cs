b973967e958004f5f279afa8cd08530206163603
Kurtina <kurtdekker@gmail.com>
2021-07-04 10:31:37 -0700

    Break Vector2Int Unity 5 helper out

---
+++ makegeo/Assets/Scripts/Vector2Int.cs
@@ -0,0 +1,15 @@
+﻿
+// later versions of Unity have a better version of this so
+// comment this struct out if your version has it...
+
+public struct Vector2Int
+{
+	public int x;
+	public int y;
+
+	public Vector2Int(int x = 0, int y = 0)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
