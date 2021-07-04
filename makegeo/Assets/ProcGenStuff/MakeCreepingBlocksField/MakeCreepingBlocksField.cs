be8706eb020dc2b2a15f9fb33fe0ba913eed6926
Kurtina <kurtdekker@gmail.com>
2021-07-04 10:29:23 -0700

    Need a Vector2Int for older Unity versions

--- makegeo/Assets/ProcGenStuff/MakeCreepingBlocksField/MakeCreepingBlocksField.cs
+++ makegeo/Assets/ProcGenStuff/MakeCreepingBlocksField/MakeCreepingBlocksField.cs
@@ -7,6 +7,13 @@ using UnityEngine;
 //	- default empty scene with light and camera
 //	- make an empty GameObject, drop this script on it.
 
+// later versions of Unity have a better version of this so
+// comment this struct out if your version has it...
+public struct Vector2Int {
+	public int x; public int y;
+	public Vector2Int(int x = 0, int y = 0) { this.x = x; this.y = y; }
+}
+
 public class MakeCreepingBlocksField : MonoBehaviour
 {
 	// TODO: you can put something in here if you like
