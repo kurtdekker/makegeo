20d3ac1b9fd7458167900180f218513407ae71b0
Kurtina <kurtdekker@gmail.com>
2022-12-27 17:24:27 -0800

    LineRendererUtility

---
+++ makegeo/Assets/2d/makeringlinerenderer/LineRendererUtility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+// @kurtdekker - just some cheap and cheerful LineRenderer-centric utils.
+
+public class LineRendererUtility
+{
+	// pass in your LineRenderer
+	//	- make sure it has a valid material
+	//	- control the width of it however you like
+	//	- make sure it is set to loop
+	public static void MakeRing( LineRenderer lineRenderer, float radius, int segments)
+	{
+		Vector3[] points = new Vector3[segments];
+
+		for (int i = 0; i < segments; i++)
+		{
+			float angle = (i * Mathf.PI * 2) / segments;
+
+			float x = Mathf.Cos( angle) * radius;
+			float y = Mathf.Sin( angle) * radius;
+
+			points[i] = new Vector3( x, y, 0);
+		}
+
+		lineRenderer.positionCount = segments;
+		lineRenderer.SetPositions( points);
+	}
+}
---
+++ makegeo/Assets/2d/makeringlinerenderer/makeringlinerenderer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class makeringlinerenderer : MonoBehaviour
+{
+	public LineRenderer lineRenderer;
+
+	public float Radius = 2.5f;
+	public int Segments = 20;
+
+	void OnValidate()
+	{
+		if (lineRenderer)
+		{
+			LineRendererUtility.MakeRing( lineRenderer: lineRenderer, radius: Radius, segments: Segments);
+		}
+	}
+}
