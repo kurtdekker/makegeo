58cde026b84c5e7c33fcb423c95817bd0d3e047d
Kurtina <kurtdekker@gmail.com>
2023-03-19 14:27:24 -0700

    Deconflicted Spawning example, 2D and 3D at the same time

---
+++ makegeo/Assets/ProcGenStuff/DeconflictedSpawning/DeconflictedSpawning2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @kurtdekker
+
+public class DeconflictedSpawning2D : MonoBehaviour
+{
+	[Header( "Make sure it has a Collider2D so we can overlap!")]
+	public GameObject PrefabToSpawn;
+
+	[Header( "Make this bigger to deconflict more.")]
+	public float CheckRadius = 0.55f;
+
+	Vector2 ChooseRandomPosition()
+	{
+		float x = Random.Range( +1.0f, +8.0f);
+		float y = Random.Range( -6.0f, +6.0f);
+		return new Vector2(x,y);
+	}
+
+	void Update()
+	{
+		Vector2 position = ChooseRandomPosition();
+
+		Collider2D result = Physics2D.OverlapCircle( position, CheckRadius);
+
+		if (!result)
+		{
+			Instantiate<GameObject>( PrefabToSpawn, position, Quaternion.identity);
+		}
+	}
+}
---
+++ makegeo/Assets/ProcGenStuff/DeconflictedSpawning/DeconflictedSpawning3D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @kurtdekker
+
+public class DeconflictedSpawning3D : MonoBehaviour
+{
+	[Header( "Make sure it has a collider so we can overlap!")]
+	public GameObject PrefabToSpawn;
+
+	[Header( "Make this bigger to deconflict more.")]
+	public float CheckRadius = 0.55f;
+
+	Vector3 ChooseRandomPosition()
+	{
+		float x = Random.Range( -8.0f, -1.0f);
+		float y = Random.Range( -6.0f, +6.0f);
+		float z = Random.Range( -3.0f, +3.0f);
+		return new Vector3(x,y,z);
+	}
+
+	void Update()
+	{
+		Vector3 position = ChooseRandomPosition();
+
+		Collider[] results = Physics.OverlapSphere( position, CheckRadius);
+
+		if (results == null || results.Length == 0)
+		{
+			Instantiate<GameObject>( PrefabToSpawn, position, Quaternion.identity);
+		}
+	}
+}
