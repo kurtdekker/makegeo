91fa8c3a57b4579ade6e8ac44924cc160fc564d1
Kurtina <kurtdekker@gmail.com>
2023-07-31 07:32:33 -0700

    PixelTerrain1

---
+++ makegeo/Assets/Pixels/PixelTerrain1/PixelTerrain1.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelTerrain1 : MonoBehaviour
+{
+	[Header( "The output renderer: we'll clone its material.")]
+	[Header( "Put a collider on it if you want to click on it.")]
+	public Renderer Target;
+
+	[Header( "Power of 2 best, may even be required!")]
+	public int Size = 64;
+
+	Material mtl;
+
+	Texture2D workTexture;
+	Color[] workPixels;
+
+	Color ColorDirt = new Color( 0.7f, 0.4f, 0.2f);	// brownish
+	Color ColorNothing = Color.black;
+
+	void Start ()
+	{
+		// backup find
+		if (!Target)
+		{
+			Target = GetComponent<Renderer>();
+		}
+
+		// complain
+		if (!Target)
+		{
+			Debug.LogError( "Did not provide Target renderer.");
+			Debug.Break();
+			return;
+		}
+
+		// get
+		mtl = Target.material;
+
+		// copy
+		mtl = new Material(mtl);
+
+		// work output
+		workTexture = new Texture2D( Size, Size);
+		workTexture.filterMode = FilterMode.Point;
+		workTexture.wrapMode = TextureWrapMode.Clamp;
+
+		// pixels
+		workPixels = workTexture.GetPixels();
+
+		// clear
+		for (int i = 0; i < workPixels.Length; i++)
+		{
+			workPixels[i] = ColorNothing;
+		}
+
+		// display
+		mtl.mainTexture = workTexture;
+		Target.material = mtl;
+	}
+
+	void UpdateClicking()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Vector3 mousePosition = Input.mousePosition;
+
+			Camera cam = Camera.main;
+
+			Ray ray = cam.ScreenPointToRay( mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast( ray: ray, hitInfo: out hit))
+			{
+				Vector2 texel = hit.textureCoord;
+
+				int x = (int)(texel.x * Size);
+				int y = (int)(texel.y * Size);
+
+				int offset = x + y * Size;
+
+				workPixels[offset] = ColorDirt;
+			}
+		}
+	}
+
+	void UpdateFallingSand()
+	{
+		// traverse from bottom up, moving pixels down-ish
+		for (int y = 1; y < Size; y++)
+		{
+			for (int x = 0; x < Size; x++)
+			{
+				int offset = x + y * Size;
+
+				Color c = workPixels[ offset];
+
+				if (c == ColorDirt)
+				{
+					// fall straight down?
+					if (workPixels[offset - Size] != ColorDirt)
+					{
+						workPixels[offset] = ColorNothing;
+						workPixels[offset - Size] = c;
+						continue;
+					}
+					// fall left diagonal
+					if (x > 0)
+					{
+						if (workPixels[offset - Size - 1] != ColorDirt)
+						{
+							workPixels[offset] = ColorNothing;
+							workPixels[offset - Size - 1] = c;
+							continue;
+						}
+					}
+					// fall right diagonal
+					if (x < Size - 1)
+					{
+						if (workPixels[offset - Size + 1] != ColorDirt)
+						{
+							workPixels[offset] = ColorNothing;
+							workPixels[offset - Size + 1] = c;
+							continue;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void UpdatePixelsToTexture()
+	{
+		workTexture.SetPixels( workPixels);
+		workTexture.Apply();
+	}
+
+	void Update ()
+	{
+		UpdateClicking();
+		UpdateFallingSand();
+		UpdatePixelsToTexture();
+	}
+}
