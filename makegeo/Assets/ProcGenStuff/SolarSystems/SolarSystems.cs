216634890a456aeb3caa1e957742ebbb5f51b81d
Kurtina <kurtdekker@gmail.com>
2023-08-08 08:27:22 -0700

    SolarSystems - click to add more of 'em

---
+++ makegeo/Assets/ProcGenStuff/SolarSystems/SolarSystems.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @kurtdekker
+
+public class SolarSystems : MonoBehaviour
+{
+	public Material baseMaterial;
+
+	public Transform GalaxyPlane;
+
+	GameObject CreateCount( int numPlanets, float maxRadius)
+	{
+		GameObject root = new GameObject( "root");
+
+		// always make a sun
+		{
+			GameObject sun = GameObject.CreatePrimitive( PrimitiveType.Sphere);
+			sun.transform.SetParent( root.transform);
+			sun.transform.localScale = Vector3.one * Random.Range( 1.0f, 2.0f);
+
+			var rndrr = sun.GetComponent<Renderer>();
+			if (rndrr)
+			{
+				Material mtl = new Material( baseMaterial);
+				mtl.color = new Color( 1.0f, Random.Range( 0.2f, 1.0f), Random.Range( 0.2f, 0.6f));
+				rndrr.material = mtl;
+			}
+		}
+
+		for (int i = 0; i < numPlanets; i++)
+		{
+			GameObject planetPivot = new GameObject("Pivot #" + i);
+			planetPivot.transform.SetParent( root.transform);
+
+			// orbit
+			{
+				float angle = Random.Range( 0.0f, 360.0f);
+				// random speed, randomly negative / positive
+				float angularSpeed = Random.Range( 10.0f, 200.0f) * (Random.Range( 0, 2) * 2 - 1);
+				SpinPlanetOnY.Attach( planetPivot, angle: angle, angularSpeed: angularSpeed);
+			}
+
+
+			GameObject planet = GameObject.CreatePrimitive( PrimitiveType.Sphere);
+			planet.transform.SetParent( planetPivot.transform);
+
+			float orbitalRadius = (maxRadius * (i + 1)) / numPlanets;
+			planet.transform.localPosition = GalaxyPlane.root.forward * orbitalRadius;
+			planet.transform.localScale = Vector3.one * Random.Range( 0.2f, 0.8f);
+
+			// spin
+			{
+				float angle = Random.Range( 0.0f, 360.0f);
+				// random speed, randomly negative / positive
+				// yes, I know that's not how orbital mechanics work. But it looks cool.
+				float angularSpeed = Random.Range( 10.0f, 50.0f) * (Random.Range( 0, 2) * 2 - 1);
+				SpinPlanetOnY.Attach( planet, angle: angle, angularSpeed: angularSpeed);
+			}
+
+			var rndrr = planet.GetComponent<Renderer>();
+			if (rndrr)
+			{
+				Material mtl = new Material( baseMaterial);
+				mtl.color = Random.ColorHSV();
+				rndrr.material = mtl;
+			}
+		}
+
+		return root;
+	}
+
+	GameObject CreateRandom()
+	{
+		int a = Random.Range( 3, 10);
+		int b = Random.Range( 3, 10);
+		int numPlanets = Mathf.Min(a,b);
+
+		return CreateCount( numPlanets, 10);
+	}
+
+	Camera cam;
+	void Start()
+	{
+		cam = Camera.main;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay( Input.mousePosition);
+
+			Plane p = new Plane( inNormal: GalaxyPlane.up, inPoint: GalaxyPlane.position);
+
+			float enter = 0;
+			if (p.Raycast( ray: ray, enter: out enter))
+			{
+				Vector3 position = ray.GetPoint( enter);
+
+				var solarSystem = CreateRandom();
+				solarSystem.transform.position = position;
+			}
+		}
+	}
+}
---
+++ makegeo/Assets/ProcGenStuff/SolarSystems/SpinPlanetOnY.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @kurtdekker - used for both orbital motion and local motion!
+
+public class SpinPlanetOnY : MonoBehaviour
+{
+	float angle;
+	float angularSpeed;
+
+	public static SpinPlanetOnY Attach( GameObject pivot, float angle, float angularSpeed)
+	{
+		var spinner = pivot.AddComponent<SpinPlanetOnY>();
+		spinner.angle = angle;
+		// note: calling Update BEFORE setting angular speed, just to set angle
+		spinner.Update();
+		spinner.angularSpeed = angularSpeed;
+		return spinner;
+	}
+
+	void Update ()
+	{
+		angle += angularSpeed * Time.deltaTime;
+
+		angle = angle % 360.0f;
+
+		transform.localRotation = Quaternion.Euler( 0, angle, 0);
+	}
+}
