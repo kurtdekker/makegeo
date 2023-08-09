cab056a202d97b62279f6d238704a776d87ddf67
Kurtina <kurtdekker@gmail.com>
2023-08-09 07:52:39 -0700

    Hex stuff: work in progress towards a hex-based dirt faller

---
+++ makegeo/Assets/Hex/HexagonalDirtfall/HexGrid.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid
+{
+	Vector2 ZeroPoint = Vector3.zero;
+
+	float CellSize = 1.0f;
+
+	float HeightRatio { get { return Mathf.Sin( Mathf.PI / 3); } }
+
+	public Vector2 SnapToGrid( Vector2 inPoint)
+	{
+		inPoint -= ZeroPoint;
+
+		float x = inPoint.x;
+		float y = inPoint.y / HeightRatio;
+
+		int iy = (int)y;
+
+		if ((iy & 1) != 0)
+		{
+			x -= 0.5f;
+		}
+
+		int ix = (int)x;
+
+		Vector2 outPoint = new Vector2( ix * CellSize, iy * CellSize * HeightRatio);
+
+		if ((iy & 1) != 0)
+		{
+			outPoint.x += 0.5f;
+		}
+
+		return outPoint;
+	}
+}
---
+++ makegeo/Assets/Hex/HexagonalDirtfall/HexagonalDirtfall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonalDirtfall : MonoBehaviour
+{
+	Camera cam;
+
+	GameObject testSphereRaw;
+	GameObject testSphereSnapped;
+
+	HexGrid hg;
+
+	void Start ()
+	{
+		cam = Camera.main;
+
+		testSphereRaw = GameObject.CreatePrimitive( PrimitiveType.Sphere);
+		testSphereSnapped = GameObject.CreatePrimitive( PrimitiveType.Sphere);
+
+		hg = new HexGrid();
+	}
+	
+	void UpdateMouse ()
+	{
+		bool fingerdown = Input.GetMouseButton(0);
+
+		Vector3 mousePosition = Input.mousePosition;
+
+		if (fingerdown)
+		{
+			Plane p = new Plane( inNormal: Vector3.back, inPoint: Vector3.zero);
+
+			Ray ray = cam.ScreenPointToRay( mousePosition);
+
+			float enter = 0;
+			if (p.Raycast( ray, out enter))
+			{
+				Vector2 point = ray.GetPoint(enter);
+
+				testSphereRaw.transform.position = point;
+
+				Vector3 snapped = hg.SnapToGrid( point);
+
+				testSphereSnapped.transform.position = snapped;
+			}
+		}
+	}
+
+	void Update()
+	{
+		UpdateMouse();
+	}
+}
