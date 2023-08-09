64e601b1b5eba12e5f672566836c9484c25e1d80
Kurtina <kurtdekker@gmail.com>
2023-08-09 08:13:22 -0700

    Hex: get the HexGrid snapping correct

--- makegeo/Assets/Hex/HexagonalDirtfall/HexGrid.cs
+++ makegeo/Assets/Hex/HexagonalDirtfall/HexGrid.cs
@@ -17,20 +17,25 @@ public class HexGrid
 		float x = inPoint.x;
 		float y = inPoint.y / HeightRatio;
 
+		x += CellSize / 2;
+		y += (CellSize * HeightRatio) / 2;
+
 		int iy = (int)y;
+		if (y < 0) iy--;
 
 		if ((iy & 1) != 0)
 		{
-			x -= 0.5f;
+			x -= CellSize / 2;
 		}
 
 		int ix = (int)x;
+		if (x < 0) ix--;
 
 		Vector2 outPoint = new Vector2( ix * CellSize, iy * CellSize * HeightRatio);
 
 		if ((iy & 1) != 0)
 		{
-			outPoint.x += 0.5f;
+			outPoint.x += CellSize / 2;
 		}
 
 		return outPoint;
